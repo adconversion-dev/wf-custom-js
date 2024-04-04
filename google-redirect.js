8db7bba19d2d50a145f5353330a529acd9ed7289
Aonnoy Sengupta <Aonnoy@Aonnoys-MacBook-Pro.local>
2024-04-04 23:36:12 +0100

    second message

---
+++ google-redirect.js
@@ -0,0 +1,10 @@
+const storedUrl = localStorage.getItem("redirectToAfterGoogle");
+if (storedUrl) {
+  const currentUrlParams = window.location.search;
+  const newUrl = new URL(storedUrl);
+  newUrl.search = currentUrlParams;
+
+  window.location.href = newUrl.href;
+} else {
+  console.log("No stored URL found for redirection.");
+}
