3289d9efc1bf6338e83660aba520c2fcbcf03154
adconversion-dev <165704888+adconversion-dev@users.noreply.github.com>
2024-04-05 23:22:00 +0100

    Update user-validation-general.js
    Code updated

--- user-validation-general.js
+++ user-validation-general.js
@@ -1,6 +1,8 @@
 // Function to check for authToken cookie
 function checkAuthToken() {
-  return document.cookie.split(";").some(item => item.trim().startsWith("authToken="));
+  return document.cookie
+    .split(";")
+    .some((item) => item.trim().startsWith("authToken="));
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -13,17 +15,17 @@ document.addEventListener("DOMContentLoaded", function () {
     // Update user's details only if authToken exists
     if (authTokenExists) {
       const userNameElement = document.querySelector('[wized="navUserName"]');
-      const userImageElement = document.querySelector('[wized="navUserImage"]'):
+      const userImageElement = document.querySelector('[wized="navUserImage"]');
 
       if (userNameElement && fullName) {
         userNameElement.textContent = JSON.parse(fullName);
-        userNameElement.removeAttribute('custom-cloak'); // Reveal the element
+        userNameElement.removeAttribute("custom-cloak"); // Reveal the element
       }
 
       if (userImageElement) {
         if (profileImageURL && profileImageURL !== "null") {
           userImageElement.src = JSON.parse(profileImageURL);
-          userImageElement.removeAttribute('custom-cloak'); // Reveal the element
+          userImageElement.removeAttribute("custom-cloak"); // Reveal the element
         } else {
           userImageElement.remove();
         }
@@ -34,17 +36,21 @@ document.addEventListener("DOMContentLoaded", function () {
   updateUserDetails(authTokenExists);
 
   // Manage visibility based on authToken existence
-  document.querySelectorAll('[custom-visibility="authenticated"]').forEach(element => {
-    if (authTokenExists) {
-      element.removeAttribute('custom-cloak'); // Reveal the element
-    }
-  });
+  document
+    .querySelectorAll('[custom-visibility="authenticated"]')
+    .forEach((element) => {
+      if (authTokenExists) {
+        element.removeAttribute("custom-cloak"); // Reveal the element
+      }
+    });
 
-  document.querySelectorAll('[custom-visibility="unauthenticated"]').forEach(element => {
-    if (!authTokenExists) {
-      element.removeAttribute('custom-cloak'); // Reveal the element
-    } else {
-      element.remove(); // Remove the element if it shouldn't be visible
-    }
-  });
+  document
+    .querySelectorAll('[custom-visibility="unauthenticated"]')
+    .forEach((element) => {
+      if (!authTokenExists) {
+        element.removeAttribute("custom-cloak"); // Reveal the element
+      } else {
+        element.remove(); // Remove the element if it shouldn't be visible
+      }
+    });
 });
