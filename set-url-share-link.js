9c4366a4f3bfadebd03a234c8edcc86679cd71aa
adconversion-dev <165704888+adconversion-dev@users.noreply.github.com>
2024-04-23 13:19:59 +0100

    Update set-url-share-link.js

--- set-url-share-link.js
+++ set-url-share-link.js
@@ -3,19 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const pageTitle = encodeURIComponent(document.title);
 
     // Set Twitter share link
-    const twitterLinks = document.querySelectorAll('[wized="socialShare_x"]');
+    const twitterLinks = document.querySelectorAll('[wized="nav_socialShareX"]');
     twitterLinks.forEach(link => {
         link.href = `https://twitter.com/intent/tweet?text=${pageTitle}&url=${pageUrl}`;
     });
 
     // Set Facebook share link
-    const facebookLinks = document.querySelectorAll('[wized="socialShare_fb"]');
+    const facebookLinks = document.querySelectorAll('[wized="nav_socialShareFb"]');
     facebookLinks.forEach(link => {
         link.href = `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}&quote=${pageTitle}`;
     });
 
     // Set LinkedIn share link
-    const linkedInLinks = document.querySelectorAll('[wized="socialShare_linkedin"]');
+    const linkedInLinks = document.querySelectorAll('[wized="nav_socialShareLinkedin"]');
     linkedInLinks.forEach(link => {
         link.href = `https://www.linkedin.com/sharing/share-offsite/?url=${pageUrl}`;
     });
