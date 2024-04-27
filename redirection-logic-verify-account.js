a100e4575f5615036692953391b847ce035d7544
adconversion-dev <165704888+adconversion-dev@users.noreply.github.com>
2024-04-27 15:49:53 +0100

    Create redirection-logic-verify-account.js

---
+++ redirection-logic-verify-account.js
@@ -0,0 +1,24 @@
+// Function to get cookie by name
+  function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+
+  // Check if the 'auth_token' cookie exists
+  const authToken = getCookie('auth_token');
+
+  // Redirect if 'auth_token' exists
+  if (authToken) {
+    window.location.href = "/auth/log-in";
+  }
