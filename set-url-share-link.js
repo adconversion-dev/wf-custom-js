b919b8073cd4ed23752b177a14d6ff3e6cdb2e9c
adconversion-dev <165704888+adconversion-dev@users.noreply.github.com>
2024-05-01 20:57:17 +0100

    Update set-url-share-link.js

--- set-url-share-link.js
+++ set-url-share-link.js
@@ -1,23 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const pageUrl = encodeURIComponent(window.location.href);
-    const pageTitle = encodeURIComponent(document.title);
+    const coursesUrl = encodeURIComponent(window.location.origin + '/courses');
 
-    // Set Twitter share link
+    // Set Twitter share link with a pretext message
+    const twitterPretext = encodeURIComponent("I'm loving #AdConversion! Join me and 1,000+ other B2B Marketers leveling up our paid ad skills...");
     const twitterLinks = document.querySelectorAll('[wized="nav_socialShareX"]');
     twitterLinks.forEach(link => {
-        link.href = `https://twitter.com/intent/tweet?text=${pageTitle}&url=${pageUrl}`;
+        link.href = `https://twitter.com/intent/tweet?text=${twitterPretext}&url=${coursesUrl}`;
     });
 
-    // Set Facebook share link
+    // Set Facebook share link without a message
     const facebookLinks = document.querySelectorAll('[wized="nav_socialShareFb"]');
     facebookLinks.forEach(link => {
-        link.href = `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}&quote=${pageTitle}`;
+        link.href = `https://www.facebook.com/sharer/sharer.php?u=${coursesUrl}`;
     });
 
-    // Set LinkedIn share link
+    // Set LinkedIn share link without a message
     const linkedInLinks = document.querySelectorAll('[wized="nav_socialShareLinkedin"]');
     linkedInLinks.forEach(link => {
-        link.href = `https://www.linkedin.com/sharing/share-offsite/?url=${pageUrl}`;
+        link.href = `https://www.linkedin.com/sharing/share-offsite/?url=${coursesUrl}`;
     });
 });
-
