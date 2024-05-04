f7759410b4aa8f546b6db9d40f8ff13fa95c102e
adconversion-dev <165704888+adconversion-dev@users.noreply.github.com>
2024-05-04 21:00:32 +0100

    Update logout.js

--- logout.js
+++ logout.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', function() {
     // Target all elements with wized="logout" attribute
     var logoutElements = document.querySelectorAll('[wized="logout"]');
 
+    // Define the base URL based on the environment
+    let baseUrl = window.location.origin.includes('server.wized.com')
+        ? '/v2/page/proxy?url=https://adconversion-academy-wized.webflow.io'
+        : window.location.origin;
+
     // Iterate over each logout element and add a click event listener
     logoutElements.forEach(function(logoutElement) {
         logoutElement.addEventListener('click', function() {
@@ -13,8 +18,9 @@ document.addEventListener('DOMContentLoaded', function() {
             localStorage.removeItem('onboarded');
             localStorage.removeItem('profile_image');
             localStorage.removeItem('course_details');
+
             // Redirect to the login page
-            window.location.href = '/auth/log-in';
+            window.location.href = `${baseUrl}/auth/log-in`;
         });
     });
 });
