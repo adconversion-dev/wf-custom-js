0390867b101d7b32b50b289a94b96955e0e37b2c
adconversion-dev <165704888+adconversion-dev@users.noreply.github.com>
2024-05-13 23:04:53 +0100

    Update google-redirect.js

--- google-redirect.js
+++ google-redirect.js
@@ -1,10 +1,1 @@
-const storedUrl = localStorage.getItem("redirectToAfterGoogle");
-if (storedUrl) {
-  const currentUrlParams = window.location.search;
-  const newUrl = new URL(storedUrl);
-  newUrl.search = currentUrlParams;
-
-  window.location.href = newUrl.href;
-} else {
-  console.log("No stored URL found for redirection.");
-}
+const storedUrl=localStorage.getItem("redirectToAfterGoogle");if(storedUrl){let e=window.location.search,o=new URL(storedUrl);o.search=e,window.location.href=o.href}else console.log("No stored URL found for redirection.");
